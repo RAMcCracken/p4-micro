2dbc7a488c1aef2e9a5cb1eb87c88d3745bd75bf
RAMcCracken <30657693+RAMcCracken@users.noreply.github.com>
2023-05-08 17:25:53 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function() {
+    basic.showIcon(IconNames.Heart)
+    pause(100)
+    basic.clearScreen()
+})
+
+input.onButtonPressed(Button.A, function() {
+    serial.writeString("a\n")
 })
